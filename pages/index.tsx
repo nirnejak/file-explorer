688de87ba75047459c2e03c12708778b7f12e52d
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-19 23:46:19 +0530

    Home: Breadcrumbs and Explorer component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react"
 
+import Explorer from "components/Explorer"
+import Breadcrumbs from "components/atoms/Breadcrumbs"
+
 interface Props {}
 
 const HomePage: React.FC<Props> = () => {
-  return <div>HomePage</div>
+  return (
+    <div>
+      <Breadcrumbs />
+      <Explorer />
+    </div>
+  )
 }
 
 export default HomePage
